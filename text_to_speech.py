6beb2687b58523662acac2a800301327b2a7bb0a
paulow540 <halleluyahpaul@gmail.com>
2025-06-24 14:25:17 +0100

    adding a streamlit app to play the audio file

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,10 +1,17 @@
 import pyttsx3
-from ppdf import my_pdf
+import streamlit as st
+from readpdf import my_pdf
 
 
 word = my_pdf()
 
 engine = pyttsx3.init()
 # # engine.say('Sally sells seashells by the seashore.')
-engine.say(word)
+engine.save_to_file(word, 'output_audio.wav')
+
 engine.runAndWait()
+st.audio("output_audio.wav")
+st.audio("python_example_test.wav")
+
+
+
